19e819a7b2a2d525486754d3b61bbc7be68715bc
chengj <chen.gj@qq.com>
2024-11-05 11:09:57 +0800

    AllowRequestHeaders

---
+++ src/ProxyMiddlewares/AllowRequestHeaders.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Oh86\GW\ProxyMiddlewares;
+
+use GuzzleHttp\Middleware;
+use Psr\Http\Message\RequestInterface;
+
+class AllowRequestHeaders extends BaseMiddleware
+{
+    /**
+     * @param array $headers
+     */
+    public function __invoke(...$headers)
+    {
+        return Middleware::mapRequest(function (RequestInterface $request) use ($headers) {
+            /** @var string $header */
+            foreach ($headers as $header) {
+                if (($val = $this->request->header($header))) {
+                    $request = $request->withHeader($header, $val);
+                }
+            }
+            return $request;
+        });
+    }
+}
