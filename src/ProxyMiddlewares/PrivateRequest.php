4f346ea0ca1e274dfc204a99b98b92161600aafa
chengj <chen.gj@qq.com>
2024-11-13 16:56:32 +0800

    PrivateRequest

--- src/ProxyMiddlewares/PrivateRequest.php
+++ src/ProxyMiddlewares/PrivateRequest.php
@@ -3,7 +3,6 @@
 namespace Oh86\GW\ProxyMiddlewares;
 
 use GuzzleHttp\Middleware;
-use Oh86\SmCryptor\Facades\Cryptor;
 use Psr\Http\Message\RequestInterface;
 
 class PrivateRequest extends BaseMiddleware
@@ -12,7 +11,7 @@ class PrivateRequest extends BaseMiddleware
     {
         return Middleware::mapRequest(function (RequestInterface $request) use ($app, $ticket) {
             $time = time();
-            $token = Cryptor::driver("local")->sm3($app . $time . $ticket);
+            $token = sm3($app . $time . $ticket);
             return $request->withHeader("Private-App", $app)
                 ->withHeader("Private-Time", $time)
                 ->withHeader("Private-Token", $token);
